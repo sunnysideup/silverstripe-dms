47d0dcf3c3afad1752ddb0ed96984720e203c484
Nicolaas <github@sunnysideup.co.nz>
2019-05-06 12:17:00 +1200

    Update DMSSiteTreeExtension.php

--- src/Extensions/DMSSiteTreeExtension.php
+++ src/Extensions/DMSSiteTreeExtension.php
@@ -18,14 +18,6 @@ use SilverStripe\ORM\DataExtension;
  * @package dms
  */
 
-/**
-  * ### @@@@ START REPLACEMENT @@@@ ###
-  * WHY: upgrade to SS4
-  * OLD:  extends DataExtension (ignore case)
-  * NEW:  extends DataExtension (COMPLEX)
-  * EXP: Check for use of $this->anyVar and replace with $this->anyVar[$this->owner->ID] or consider turning the class into a trait
-  * ### @@@@ STOP REPLACEMENT @@@@ ###
-  */
 class DMSSiteTreeExtension extends DataExtension
 {
     private static $has_many = array(
