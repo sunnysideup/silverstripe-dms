70ab5a825c6445ff98f0312fd60b7b825e12ae8f
LABCAT <shane.watters.nz@gmail.com>
2019-05-10 17:39:18 +1200

    MINOR: adding the functionality required to convert file to DMSDocument if it is in the dmsassets folder.

--- src/Extensions/FileExtension.php
+++ src/Extensions/FileExtension.php
@@ -3,9 +3,11 @@
 namespace Sunnysideup\DMS\Extensions;
 
 
+use SilverStripe\Assets\Folder;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\ORM\DataExtension;
 
+use Sunnysideup\DMS\Model\DMSDocument;
 /**
  * Creates default taxonomy type records if they don't exist already
  */
@@ -21,4 +23,16 @@ class FileExtension extends DataExtension
         'OriginalDMSDocumentID' => true
     ];
 
+
+    /**
+     * Event handler called before writing to the database.
+     */
+    public function onBeforePublish()
+    {
+        $dmsFolder = Folder::find_or_make('dmsassets');
+        if($this->owner->ParentID === $dmsFolder->ID){
+            $this->owner->ClassName = DMSDocument::class;
+            $this->owner->write();
+        }
+    }
 }
